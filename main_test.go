a0c537a8f870470b164fbd4bb6ac82a5dc771605
Jack Treble <Jack.Treble@ovoenergy.com>
2018-07-30 10:20:25 +0100

    address code reveiew comments

--- main_test.go
+++ main_test.go
@@ -17,7 +17,7 @@ func TestZerofill(t *testing.T) {
 	er := zerofill(path)
 	check(er)
 
-	if _, err := os.Stat(path); !os.IsNotExist(err) {
+	if _, err := os.Stat(path); err == nil {
 		t.Error("file still exists")
 	}
 
